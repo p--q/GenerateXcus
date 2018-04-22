68953c10ee86963a56f05af114a09a6fa1e70b04
pq <pq.blogspot@gmail.com>
2018-04-23 00:07:11 +0900

    HM

--- GenerateXcus/src/ini2xcu.py
+++ GenerateXcus/src/ini2xcu.py
@@ -1,8 +1,10 @@
 #!/opt/libreoffice5.4/program/python
 # -*- coding: utf-8 -*-
-import os, glob
-# from xml.etree import ElementTree
+import os, glob, sys, re
+from xml.etree import ElementTree
+from xml.etree.ElementTree import Element
 from configparser import ConfigParser
+from xml.dom import minidom
 def main():
 	pwd = os.path.dirname(os.path.abspath(__file__))  # このスクリプトのあるフォルダのパスを取得。
 	outfolder = os.path.join(pwd, "xcu")  # 出力先フォルダのパスの取得。
@@ -10,14 +12,87 @@ def main():
 		os.makedirs(outfolder)  # 出力先フォルダを作成。	
 	inifolder = os.path.join(pwd, "ini")  # ソースフォルダのパスの取得。
 	os.chdir(inifolder)  # iniファイルのあるフォルダに移動。
-	for i in glob.iglob("*.ini"):
-		config = ConfigParser()
-		config.read(i)
+	inidic = {i.rsplit(".", 1)[0]: ConfigParser().read(i) for i in glob.iglob("*.ini")}
+	ns = {"oor": "http://openoffice.org/2001/registry", "xs": "http://www.w3.org/2001/XMLSchema", "xsi": "http://www.w3.org/2001/XMLSchema-instance"}
+	ElementTree.register_namespace("oor", ns["oor"])  # 名前空間の設定。出力する時に使用される。
+	getAttr, delAttr = attrfuncCreator(ns)
+	for name, config in inidic.items():
+		schemafile = os.path.join(pwd, "xml", "{}.xml".format(name))
+		if os.path.exists(schemafile):
+			tree = ElementTree.parse(schemafile)	
+		else:
+			print("There is no xml file expanding the templates node of the component-schema node.", file=sys.stderr)
+			sys.exit()
+		schema = tree.getroot()
+		component = schema.find("component", ns)
+		data = createElem("{{{}}}component-data".format(ns["oor"]))
+		data.extend([i for i in component])
+		parentmap = {c:p for p in data.iter() for c in p}  # キー: ノード, 値: 親ノード、の辞書。
+		for n in data.iter():
+			tag = n.add
+			name = getAttr(n, "name")
+			if tag=="set":
+				delAttr(n, "node-type") 
+				sectionbase = "{} set".format(" ".join(getSetNames(parentmap, n)))
+				c = 1
+				section = "{}{}".format(sectionbase, c)
+				while section in config:
+					section = "{}{}".format(sectionbase, c)
 		
 		
 		
-		for k, v in config.items():
-			print("key: {}, val: {}".format(k, v))
+					c += 1
+				n.tag = "node"
+
+				
+	
+			
+		
+		
+		[data.set(k, v) for k, v in schema.items() if not "lang" in k]
+		data.set("xmlns:xs", ns["xs"])
+		data.set("xmlns:xsi", ns["xsi"])		
+		filename = ".".join([name, "xcu"])		
+		writeFile(data, outfolder, filename, reset=True)  # ファイルを書き出す。	
+def getSetNames(parentmap, n):  # ルートからノードnまでのセットノードのname属性のリストを取得する。
+	names = []  # ルートまでのセットノードのname属性を入れるリスト。
+	c = n
+	while c in parentmap:  # 親ノードがある間。
+		if c.tag=="set":  # セットノードの時。
+			names.append(c.get("name"))
+		c = parentmap[c]  # 親ノードについて調べる。	
+	return names[::-1]  # ルートノードからの順にして返す。
+def attrfuncCreator(ns): 	
+	def _nsattr(name):
+		return "".join("{", ns["oor"], "}", name)
+	def getAttr(n, name):
+		return n.get(_nsattr(name))
+	def delAttr(n, name):
+		return n.attrib.pop(_nsattr(name), None)  # 第2引数を渡すとキーがなくてもエラーが出ない。
+	return getAttr, delAttr			
+def createElem(tag, attrib={},  **kwargs):  # ET.Elementのアトリビュートのtextとtailはkwargsで渡す。		
+	txt = kwargs.pop("text", None)
+	tail = kwargs.pop("tail", None)
+	sub = kwargs.pop("sub", None)  # サブノードにするET.Elementを取得。
+	subs = kwargs.pop("subs", None)  # サブノードにするET.Elementのタプルを取得。
+	elem = Element(tag, attrib, **kwargs)
+	if txt is not None:
+		elem.text = txt
+	if tail is not None:
+		elem.tail = tail	
+	if sub is not None:  # ET.Elementが入っていてもsubだけだとFalseになる。
+		elem.append(sub)
+	if subs is not None:
+		elem.extend(subs)	
+	return elem	 # Elementオブジェクトを返す。		
+def writeFile(schema, outfolder, filename, *, reset=None):  # xcsファイルとして書き出す。			
+	x = ElementTree.tostring(schema, encoding="unicode")  # ElementTreeをXML文字列に変換。
+	if reset is not None:  # reset引数がある時。
+		x = re.sub(r'(?<=>)\s+?(?=<)', "", x)  # 空文字だけのtextとtailを削除してすでにあるインデントをリセットする。
+	with open(os.path.join(outfolder, filename), "w", encoding="utf-8") as f:
+		f.write(minidom.parseString(x).toprettyxml())  # XMLを整形して書き出す。			
+
+
 		
 		
 # 		with open(i, encoding="utf-8") as f:
